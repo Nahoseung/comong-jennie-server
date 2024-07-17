17bb5ca5e03e74bfa7bb4953306835f960737d28
root <root@BOOK-PV6LKIFM9B.localdomain>
2024-07-18 02:11:29 +0900

    python-version

--- comong_project/User/urls.py
+++ comong_project/User/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import urls
 
 urlpatterns = [
     path("",views.index,name="index"),
     path("users/<int:user_id>/",views.ProfileView.as_view(),name="profile"),
+    path('signup/', views.UserCreate.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
