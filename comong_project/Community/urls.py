78ef0ace494708bc278dfb787936fb45f5cb5e07
Nahoseung <wbc2104@g.hongik.ac.kr>
2024-07-17 21:49:29 +0900

    generic model view set 활용 게시물 CRUD 구현

--- comong_project/Community/urls.py
+++ comong_project/Community/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 # from .views import grade,sub,prof,posts,post_detail
-from .views import post_list, post_deatil
+from .views import post
+
+router = DefaultRouter()
+router.register('post', post)  
+
+# router url 종류 파악 필요
+
 urlpatterns = [
+    path('', include(router.urls)),
     # path('grade/', grade),
     # path('sub/',sub),
     # path('prof/',prof),
-    path('posts/',post_list),
-    path('posts/<int:pk>/', post_deatil),
+    # path('posts/',post_list),
+    # path('posts/<int:pk>/', post_deatil),
 ]
--- comong_project/Community/views.py
+++ comong_project/Community/views.py
@@ -4,19 +4,32 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Post
 from .serializers import PostlistSerializer, PostdetailSerializer
 
-@api_view(['GET'])
-def post_list(request):
-    posts=Post.objects.all()
-    serializer=PostlistSerializer(posts,many=True)
-    return Response(serializer.data, status=200)
+
+# @api_view(['GET'])
+# def post_list(request):
+#     posts=Post.objects.all()
+#     serializer=PostlistSerializer(posts,many=True)
+#     return Response(serializer.data, status=200)
 
 
-@api_view(['GET'])
-def post_deatil(request,pk):
-    posts_detail=Post.objects.get(id=pk)
-    serializer=PostdetailSerializer(posts_detail) # post object is not iterable
-    return Response(serializer.data, status=200)
+    
+# @api_view(['GET'])
+# def post_deatil(request,pk):
+#     posts_detail=Post.objects.get(id=pk)
+#     serializer=PostdetailSerializer(posts_detail) # post object is not iterable
+#     return Response(serializer.data, status=200)
+
+
+class post(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostdetailSerializer
+    
+    
+# post_list에서는 제목,작성자(+프로필),시간,태그만 보이도록(content 제외) 세부 기능 구현
+# ModelViewset 조금 더 공부 필요
