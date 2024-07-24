87861c22c6e12eb57bd11e2a0c0922cd758591c1
Nahoseung <wbc2104@g.hongik.ac.kr>
2024-07-24 16:54:54 +0900

    Community 태그 모델 추가+ 관계 초기 세팅

--- comong_project/Community/models.py
+++ comong_project/Community/models.py
@@ -1,20 +1,35 @@
 from django.db import models
-# from User.model import User
+from User.models import User
+# from .models import Board, HashTag
 # Create your models here.
-
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    # author= User
-    # tag = models.CharField(max_length=10)
-    # likes = 
-    dt_created = models.DateTimeField(verbose_name="Date Created",auto_now_add=True)
-    dt_modified = models.DateTimeField(verbose_name="Date Modified",auto_now=True)
-
 
 class Board(models.Model):
     grade = models.IntegerField()
     sub = models.CharField(max_length=20)
     profs = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return '{0}/{1}/{2}'.format(self.grade,self.sub,self.profs)
+    
+class HashTag(models.Model):
+    tag = models.TextField()
+    
+    def __str__(self):
+        return self.content
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    author= models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+    tag = models.ManyToManyField(HashTag,blank=True)
+    dt_created = models.DateTimeField(verbose_name="Date Created",auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified",auto_now=True)
+    # board = models.OneToOneField(Board )
+    
+    def __str__(self):
+        return self.title
+
+
+
 
 
